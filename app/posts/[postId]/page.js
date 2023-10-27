1aa3e37e3f3996713a484f840e6746a225a7e7b0
Brad <bmpalagi@gmail.com>
2023-10-26 19:59:41 -0500

    trying github copilot

---
+++ app/posts/[postId]/page.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+export default function EmailPage() {
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`Email submitted: ${email}`);
+        // Add code here to send email to your email list
+    };
+
+    const handleChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    return (
+        <div>
+            <h1>Join our email list</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Email:
+                    <input type="text" value={email} onChange={handleChange} />
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
